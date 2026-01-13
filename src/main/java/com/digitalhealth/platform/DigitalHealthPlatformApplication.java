a99847531dfebe3dd59611cb48c2394a53fed848
lokesh <lokeshkumargithub@gmail.com>
2026-01-13 11:15:12 +0530

    Initial setup - digital health platform

---
+++ src/main/java/com/digitalhealth/platform/DigitalHealthPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.digitalhealth.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalHealthPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalHealthPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalhealth/platform/DigitalHealthPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalhealth.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalHealthPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
