d37995428df0fb7a14558f3369967007b08ee82f
lokesh <lokeshkumargithub@gmail.com>
2026-02-02 21:54:41 +0530

    feat(notification): add SMTP email configuration and implement notification service

--- src/main/java/com/digitalhealth/platform/DigitalHealthPlatformApplication.java
+++ src/main/java/com/digitalhealth/platform/DigitalHealthPlatformApplication.java
@@ -1,13 +1,50 @@
 package com.digitalhealth.platform;
 
+import com.digitalhealth.platform.notification.dto.NotificationCreateRequest;
+import com.digitalhealth.platform.notification.service.NotificationService;
+import com.digitalhealth.platform.users.entity.User;
+import com.digitalhealth.platform.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@EnableAsync
+@RequiredArgsConstructor
 public class DigitalHealthPlatformApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalHealthPlatformApplication.class, args);
 	}
 
+//
+//	@Bean
+//	CommandLineRunner runner(UserRepository userRepository, NotificationService notificationService){
+//		return args -> {
+//
+//			User user = userRepository.findById(5L)
+//					.orElseThrow(() -> new RuntimeException("User not found"));
+//
+//			Map<String, Object> variables = new HashMap<>();
+//			variables.put("name", "Lokesh");
+//			variables.put("resetLink", "https://app.digitalhealth.com/reset?token=abc123");
+//
+//			NotificationCreateRequest request = NotificationCreateRequest.builder()
+//					.recipient("lokeshkumawat1225@gmail.com")
+//					.subject("Testing Email")
+//					.message("Hey, this is a test mail")
+//					.templateName("password-reset")
+//					.templateVariables(variables)
+//					.build();
+//			notificationService.sendEmail(request, user);
+//		};
+//	}
 }
---
+++ src/main/java/com/digitalhealth/platform/notification/service/NotificationService.java
@@ -0,0 +1,85 @@
+package com.digitalhealth.platform.notification.service;
+
+import com.digitalhealth.platform.common.enums.NotificationType;
+import com.digitalhealth.platform.notification.dto.NotificationCreateRequest;
+import com.digitalhealth.platform.notification.entity.Notification;
+import com.digitalhealth.platform.notification.repository.NotificationRepository;
+import com.digitalhealth.platform.users.entity.User;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.nio.charset.StandardCharsets;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+
+    private final NotificationRepository notificationRepo;
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    @Async
+    public void sendEmail(NotificationCreateRequest request, User user) {
+
+        try {
+
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+            MimeMessageHelper helper = new MimeMessageHelper(
+                    mimeMessage,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name()
+            );
+
+            helper.setTo(request.getRecipient());
+            helper.setSubject(request.getSubject());
+
+            // Use template if provided
+            if (request.getTemplateName() != null) {
+                Context context = new Context();
+                context.setVariables(request.getTemplateVariables());
+
+                String htmlContent = templateEngine.process(
+                        request.getTemplateName(),
+                        context
+                );
+
+                helper.setText(htmlContent, true);
+            } else {
+                helper.setText(request.getMessage(), true);
+            }
+
+            mailSender.send(mimeMessage);
+
+            log.info("Email sent successfully to={}, traceId={}",
+                    request.getRecipient(),
+                    MDC.get("traceId"));
+
+            // Save notification
+            Notification notification = Notification.builder()
+                    .recipient(request.getRecipient())
+                    .subject(request.getSubject())
+                    .message(request.getMessage())
+                    .type(NotificationType.EMAIL)
+                    .user(user)
+                    .build();
+
+            notificationRepo.save(notification);
+
+        } catch (Exception e) {
+            // keeping it SIMPLE as you asked
+            log.error("Failed to send email, traceId={}", MDC.get("traceId"), e);
+        }
+    }
+}
